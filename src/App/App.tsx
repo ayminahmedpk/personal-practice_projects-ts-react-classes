499e6ea19ed4c56822e26eca5cd0840dbb35c0ca
ayminahmedpk <ayminahmedpk@gmail.com>
2022-08-03 09:51:38 +0500

    0.1.11

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import Grandparent from "../Lessons/02-other-features/02-portals/02-portals-and-rerendering/Grandparent";
+import Parent from "../Lessons/01-core-react-basics/03-children-outside-jsx/Parent";
+// import Grandparent from "../Lessons/02-other-features/02-portals/02-portals-and-rerendering/Grandparent";
 
 import './App.css';
 
@@ -8,7 +9,7 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-        <Grandparent/>
+        <Parent/>
       </>
     )
   }
--- src/Lessons/01-core-react-basics/03-children-outside-jsx/Child.tsx
+++ src/Lessons/01-core-react-basics/03-children-outside-jsx/Child.tsx
@@ -1,17 +1,26 @@
 
 
 import React, { Component } from 'react'
+import {PropsWithChildren} from 'react';
 
-type ChildProps = {
-  children: React.ReactNode,
-}
+// type ChildProps = {
+//   // nonexistent?: string,  // Optional due to ?
+//   children: React.ReactNode,
+// }
 
-export default class Child extends Component<ChildProps> {
+
+// If we had props besides children, we could define the normal props within
+// ChildProps, and instead extend Child as
+//      Component<PropsWithChildren<ChildProps>> {...}
+
+// export default class Child extends Component<PropsWithChildren<ChildProps>> {
+export default class Child extends Component<PropsWithChildren> {
   name: string = 'Child';
   render() {
     return (
       <div className="component">
         <p className="component__name">{this.name}</p>
+        {/* {this.props.nonexistent ? this.props.nonexistent : ''} */}
         {this.props.children}
       </div>
     )
